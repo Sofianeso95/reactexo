efcc27889b44ccedec8579c27cd9460f3d4699cf
sofianeso95 <sofiane.chila@my-digital-school.org>
2023-05-11 16:59:05 +0200

    exo1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Calculette from './Calculette';
+
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <Calculette />
+
+    </div>
+
+  );
+
+}
+
+export default App;
+
+
+
+
+
